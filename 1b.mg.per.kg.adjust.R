b5fd421d0cef85f2d47f8730ad1485f49106616f
LeeKennedy <mail@leekennedy.id.au>
2015-02-15 20:06:21 +1100

    Refining script

--- 1b.mg.per.kg.adjust.R
+++ 1b.mg.per.kg.adjust.R
@@ -3,7 +3,7 @@
 rm(list=ls())
 
 # Import File ----------------------------------------------------------------------------- 
-lims <- read.csv("ICPM01-Lead.csv", stringsAsFactors = FALSE,header = TRUE)
+lims <- read.csv("CHLN01-Choline.csv", stringsAsFactors = FALSE,header = TRUE)
 
 colnames(lims)[1] <- 'SAMPLE_NUMBER'
 
