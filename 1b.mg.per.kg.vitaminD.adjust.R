d47670425a66f160085b11c089354f8b89cb49e4
LeeKennedy <mail@leekennedy.id.au>
2015-02-20 23:02:27 +1100

    Vitamin D specific script

---
+++ 1b.mg.per.kg.vitaminD.adjust.R
@@ -0,0 +1,37 @@
+### CHANGE ###: file name 
+lims <- read.csv("VITD01-Vitamin D.csv", as.is=TRUE,header = TRUE)
+
+colnames(lims)[1] <- 'SAMPLE_NUMBER'
+
+#Converts all units to mg/kg
+units <- "MG_P_KG"
+
+unit <- which((lims$UNITS=="G_P_100G"))
+lims$UNITS[unit] <- "MG_P_KG"
+lims$ENTRY[unit] <- as.numeric(lims$ENTRY[unit]) * 10000
+
+unit <- which((lims$UNITS=="MG_P_100G"))
+lims$UNITS[unit] <- "MG_P_KG"
+lims$ENTRY[unit] <- as.numeric(lims$ENTRY[unit]) * 10
+
+unit <- which((lims$UNITS=="PCT_M-M"))
+lims$UNITS[unit] <- "MG_P_KG"
+lims$ENTRY[unit] <- as.numeric(lims$ENTRY[unit]) * 10000
+
+unit <- which((lims$UNITS=="UG_P_100G"))
+lims$UNITS[unit] <- "MG_P_KG"
+lims$ENTRY[unit] <- as.numeric(lims$ENTRY[unit]) * 0.01
+
+unit <- which((lims$UNITS=="MG_P_G"))
+lims$UNITS[unit] <- "MG_P_KG"
+lims$ENTRY[unit] <- as.numeric(lims$ENTRY[unit]) * 1000
+
+unit <- which((lims$UNITS=="IU_P_100G"))
+lims$UNITS[unit] <- "MG_P_KG"
+lims$ENTRY[unit] <- as.numeric(lims$ENTRY[unit]) * 0.00025
+
+lims2 <- lims[which(lims$UNITS %in% "MG_P_KG"),]
+
+#Omits any remaining items with irregular units.
+write.csv(lims2, file = "clean.units.csv", row.names = FALSE)
+
