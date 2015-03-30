2a035393735395d56f6a1fdae7dd1ce6ab896457
LeeKennedy <mail@leekennedy.id.au>
2015-03-30 21:52:17 +1100

    Minor additions

--- analyte.filter.R
+++ analyte.filter.R
@@ -1,23 +1,21 @@
 # Clear Environment -----------------------------------------------------------------------
 rm(list=ls())
 
-<<<<<<< HEAD
-data <- read.csv("MOIS22.csv", as.is=TRUE, header=TRUE)
-=======
-data <- read.csv("MICP06-Zinc.csv", as.is=TRUE, header=TRUE)
->>>>>>> origin/master
+
+data <- read.csv("TITA13.csv", as.is=TRUE, header=TRUE)
+
 colnames(data)[1] <- "SAMPLE_NUMBER"
 library(dplyr)
 
 table(data$REPORTED_NAME)
 unique(data$UNITS)
-
+unique(data$ANALYSIS)
 
 data$REPORTED_NAME <- "Lactic Acid"
 
 # Split & recalculate --------------------------------------------------------------------
 data.A <- data %>%
-        filter(REPORTED_NAME == "L Lactic Acid")
+        filter(REPORTED_NAME == "Titratable Acidity ( as Lactic Acid )")
 
 data.B <- data %>%
         filter(REPORTED_NAME == "Moisture")%>%
@@ -28,17 +26,13 @@ data.c <- rbind(data.A, data.B)
 write.csv(data.A, "LACA-L/LACA02L.csv", row.names=FALSE)
 
 # Filtering -----------------------------------------------------------------------------
+
+analysis <- "TITA12"
 data.F <- data %>%
-<<<<<<< HEAD
-        filter(grepl("VERICAL LOW",DESCRIPTION)==FALSE)%>%
-        filter(grepl("Palmitate", REPORTED_NAME)==FALSE)%>%
+        filter(grepl(analysis,ANALYSIS)==TRUE)
+        #filter(grepl("Palmitate", REPORTED_NAME)==FALSE)%>%
         #filter(REPORTED_NAME=="Sorbic Acid")
-        filter(grepl("SO2", REPORTED_NAME)==TRUE)
-data.F$REPORTED_NAME <- data.F$REPORTED_NAME[1]
-write.csv(data.F, "Verical/MOIS22.csv", row.names=FALSE)
-=======
-        #filter(grepl("Acetate",REPORTED_NAME)==FALSE)%>%
-        filter(grepl("Zinc", REPORTED_NAME)==TRUE)
-data.F$REPORTED_NAME <- data.F$REPORTED_NAME[1]
-write.csv(data.F, "Zinc_a.csv", row.names=FALSE)
->>>>>>> origin/master
+        #filter(grepl("SO2", REPORTED_NAME)==TRUE)
+#data.F$REPORTED_NAME <- data.F$REPORTED_NAME[1]
+write.csv(data.F, paste(analysis, ".csv", sep=""), row.names=FALSE)
+
