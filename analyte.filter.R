375f08a7aa7e23b13a4195a1bb9513c200637491
LeeKennedy <mail@leekennedy.id.au>
2015-11-09 18:08:54 +1100

    Routine fiddling

--- analyte.filter.R
+++ analyte.filter.R
@@ -1,13 +1,12 @@
 # Clear Environment ---------------------------------------------
 rm(list=ls())
+library (dplyr)
 
 
-lims <- read.csv("MOIS01a.csv", as.is=TRUE, header=TRUE)
+lims <- read.csv("Molybdenum.csv", as.is=TRUE, header=TRUE)
 
 
 colnames(lims)[1] <- "SAMPLE_NUMBER"
-library(dplyr)
-
 
 table(lims$REPORTED_NAME)
 unique(lims$UNITS)
@@ -20,5 +19,14 @@ unique(lims$ANALYSIS)
 
 
 lims <- lims %>%
-        filter(ANALYSIS == "MICP010406")
 
+        filter(PRODUCT_GRADE == "EAP00701" |
+                       PRODUCT_GRADE == "EAP00702" |
+                       PRODUCT_GRADE == "EAP00703" |
+                       PRODUCT_GRADE == "EAP00700" |
+                       PRODUCT_GRADE == "EAP00717")
+
+
+lims <- lims %>%
+        #filter(grepl("6.38",REPORTED_NAME)
+        filter(REPORTED_NAME == "Total Solids")
