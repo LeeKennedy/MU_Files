d9b0011835ffdca98689ac692bad3b048d00ff87
LeeKennedy <mail@leekennedy.id.au>
2016-02-05 17:58:33 +1100

    Adjust excel export

--- save.to.excel.R
+++ save.to.excel.R
@@ -3,14 +3,16 @@ library(xlsx) #load the package
 
 df <- read.csv(paste(file.name, ".csv", sep=""), header = TRUE, as.is = TRUE)
 
-df2 <- loadWorkbook("CODExx-analyte-Filterable.xlsx") 
+df2 <- loadWorkbook("/Users/Study Old/Desktop/2016 MU Transfer/CODExx-analyte-Filterable.xlsx") 
 
 df_s <- getSheets(df2)
+
+Method.code <- df$Analysis[1]
 
 addDataFrame(df, sheet = df_s$Data, row.names = FALSE, col.names = FALSE, startRow=11, startColumn = 1) 
 
 # write data to sheet starting on line 1, column 4
-saveWorkbook(df2, paste(Method.code, "-Filterable.xlsx")) # and of course you need to save it.
+saveWorkbook(df2, paste(Method.code, "-Filterable.xlsx", sep="")) # and of course you need to save it.
 
 #----------------------
 
