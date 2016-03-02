cb1af24afef232cb032a4b57a13211bf0658f48b
LeeKennedy <mail@leekennedy.id.au>
2016-03-02 19:40:46 +1100

    Misc changes

--- lib/helpers.R
+++ lib/helpers.R
@@ -2,3 +2,30 @@ helper.function <- function()
 {
   return(1)
 }
+#------------------------------------------------------------------------
+summary_data <- function(x) {
+        colnames(x)[1] <- "SAMPLE_NUMBER"
+        
+        #----------------------------------------------------------------------
+        testcodes <- length(unique(x$ANALYSIS))
+        if (testcodes > 1) {
+                message("More than one test code present")
+                stop()
+        }
+        #----------------------------------------------------------------------
+        
+        
+        reported_names <- unique(x$REPORTED_NAME)
+        names <- as.data.frame(reported_names)
+        print (names)
+        
+        #----------------------------------------------------------------------
+        print("------------------------------")
+        
+        units_1 <- table(x$UNITS)
+        units_2 <- as.data.frame(units_1)
+        print(units_2)
+        
+}
+
+#------------------------------------------------------------------------
--- scripts/clean.up.R
+++ scripts/clean.up.R
@@ -1,34 +1,10 @@
 # Clear Environment ---------------------------------------------
 rm(list=ls())
-library (dplyr)
+library (ProjectTemplate)
+load.project()
 
+lims <- read.csv("TITA14.csv", as.is=TRUE, header=TRUE)
 
-lims <- read.csv("PROT01.csv", as.is=TRUE, header=TRUE)
-
-summary_data <- function(x) {
-colnames(x)[1] <- "SAMPLE_NUMBER"
-
-#----------------------------------------------------------------------
-testcodes <- length(unique(x$ANALYSIS))
-if (testcodes > 1) {
-        message("More than one test code present")
-        stop()
-}
-#----------------------------------------------------------------------
-
-
-reported_names <- unique(x$REPORTED_NAME)
-names <- as.data.frame(reported_names)
-print (names)
-
-#----------------------------------------------------------------------
-print("------------------------------")
-
-units_1 <- table(x$UNITS)
-units_2 <- as.data.frame(units_1)
-print(units_2)
-
-}
 
 summary_data(lims)
 
@@ -37,4 +13,7 @@ lims <- lims %>%
         filter(UNITS == "MG_P_L")
 
 lims <- lims %>%
-        filter(REPORTED_NAME == "Protein (TN x 6.38)")
+        filter(REPORTED_NAME == "Titratable Acidity ºT")
+
+lims <- lims %>%
+        filter(grepl("pH", REPORTED_NAME))
