0756dbba767b5111ee0fce52fc12e3a8698a381b
LeeKennedy <mail@leekennedy.id.au>
2017-08-22 19:59:30 +1000

    Used outlier routine to trim bias of, well, outliers.

--- scripts/100_MU_bias.R
+++ scripts/100_MU_bias.R
@@ -4,17 +4,7 @@ rm(list=ls())
 # Packages ---------------------------------------------------------------
 library(tidyverse)
 library(readxl)
-
-# Functions --------------------------------------------------------------
-
-remove_outliers <- function(x, na.rm = TRUE, ...) {
-        qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
-        H <- 1.5 * IQR(x, na.rm = na.rm)
-        y <- x
-        y[x < (qnt[1] - H)] <- NA
-        y[x > (qnt[2] + H)] <- NA
-        y
-}
+library(dts.quality)
 
 # Data Input -------------------------------------------------------------
 
@@ -27,12 +17,19 @@ bias <- bias[-c(1:9),1:14]
 
 colnames(bias) <- c("Type", "Reference", "Date", "Test_Code", "Analyte", "Unit", "Matrix","Reference_Mean", "sd","no","Lab_Result","pct_sd", "Bias", "pct_Bias")
 
-# Bias-----------------------------------------------------------
+
+# Data Cleaning ----------------------------------------------------------
+
+
+
+
+# Bias--------------------------------------------------------------------
 
 
 summary_bias <- bias %>% 
         group_by(Test_Code, Analyte, Matrix) %>% 
-        summarize(n = n(), u_ref = mean(pct_sd)/sqrt(mean(no)), av_bias = mean(pct_Bias), sd_bias = sd(pct_Bias),UoB = sqrt(u_ref^2 + sd_bias^2))
+        mutate(pct_Bias = outliers(pct_Bias)) %>% 
+        summarize(n = sum(!is.na(pct_Bias)), u_ref = mean(pct_sd, na.rm = TRUE)/sqrt(mean(no, na.rm = TRUE)), av_bias = mean(pct_Bias, na.rm = TRUE), sd_bias = sd(pct_Bias, na.rm = TRUE),UoB = sqrt(u_ref^2 + sd_bias^2))
 
 
 summary_bias <- summary_bias %>% 
@@ -41,4 +38,4 @@ summary_bias <- summary_bias[,c(1,2,3,4,6,8)]
 
 summary_bias
 
-write.csv(summary_bias, "MU_Bias_Summary.csv")
+write.csv(summary_bias, "MU_Bias_Summary2.csv")
