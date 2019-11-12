3ccbc150e7fa1e669ff8912e0a236b961fb87952
LeeKennedy <mail@leekennedy.id.au>
2019-11-13 08:06:06 +1100

    FATS03

--- scripts/10_clean.up.R
+++ scripts/10_clean.up.R
@@ -35,7 +35,7 @@ print(units_2)
 
 # Program -------------------------------------------------------------
 
-lims <- read_csv("liq_milk.csv")
+lims <- read_csv("FATS03.csv")
 lims <- strip_mm(lims)
 
 summary_data(lims)
