755b1b0d3035629d60ca6e0a469bffc39f8f054c
LeeKennedy <mail@leekennedy.id.au>
2019-12-31 13:57:14 +1100

    Update 10_clean.up.R

--- scripts/10_clean.up.R
+++ scripts/10_clean.up.R
@@ -35,7 +35,7 @@ print(units_2)
 
 # Program -------------------------------------------------------------
 
-lims <- read_csv("Bismuth.csv")
+lims <- read_csv("Tin.csv")
 lims <- strip_mm(lims)
 
 summary_data(lims)
