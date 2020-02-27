5f35160b846f74ffef45dc106f28cd65c4d51384
LeeKennedy <mail@leekennedy.id.au>
2020-02-28 09:32:00 +1100

    Update 10_clean.up.R

--- scripts/10_clean.up.R
+++ scripts/10_clean.up.R
@@ -35,7 +35,7 @@ print(units_2)
 
 # Program -------------------------------------------------------------
 
-lims <- read_csv("FATS17.csv")
+lims <- read_csv("OLIG05.csv")
 lims <- strip_mm(lims)
 
 summary_data(lims)
